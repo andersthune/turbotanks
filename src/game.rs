53be7ee0fdc4df12cac6e5f4def2bed2765e21d7
Anders Thuné <athune@kth.se>
2019-05-20 22:50:54 +0200

    Move Game to own file

---
+++ src/game.rs
@@ -0,0 +1,55 @@
+use crate::settings::Settings;
+use sfml::graphics::{CircleShape, RenderTarget, RenderWindow, Transformable};
+use sfml::system::{Vector2f, Vector2u};
+use sfml::window::{Event, Style};
+
+pub struct Game {
+    window: RenderWindow,
+    settings_path: &'static str,
+    settings: Settings,
+}
+
+impl Game {
+    pub fn new(settings_path: &'static str) -> Game {
+        let settings = Settings::new(settings_path);
+
+        let mut window = RenderWindow::new(
+            settings.get_resolution(),
+            "Turbo Tanks",
+            Style::DEFAULT,
+            &Default::default(),
+        );
+
+        window.set_framerate_limit(settings.framerate_limit);
+
+        Game {
+            window,
+            settings_path,
+            settings,
+        }
+    }
+
+    pub fn running(&self) -> bool {
+        self.window.is_open()
+    }
+
+    pub fn update(&mut self) {
+        while let Some(event) = self.window.poll_event() {
+            if let Event::Closed = event {
+                self.window.close()
+            }
+        }
+        let mut circle = CircleShape::new(10.0, 50);
+        let Vector2u { x, y } = self.window.size();
+        let x = (x as f32) / 2.0;
+        let y = (y as f32) / 2.0;
+
+        circle.set_position(Vector2f { x, y });
+
+        self.window.set_active(true);
+
+        self.window.draw(&circle);
+
+        self.window.display()
+    }
+}
