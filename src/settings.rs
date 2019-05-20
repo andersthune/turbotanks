3b905cb7f844d57a6e1e4736f68b605d51af0791
Anders Thuné <athune@kth.se>
2019-05-20 23:04:18 +0200

    Modify settings docstrings to clarify use of TOML format

--- src/settings.rs
+++ src/settings.rs
@@ -21,14 +21,14 @@ pub struct Settings {
 }
 
 impl Settings {
-    /// Create a new Settings object from the toml file at
+    /// Create a new Settings object from the TOML file at
     /// `settings_path`. Will return default settings if the file
     /// couldn't be loaded.
     pub fn new(settings_path: &'static str) -> Settings {
         toml::from_str(&fs::read_to_string(settings_path).unwrap_or_default()).unwrap_or_default()
     }
 
-    /// Write settings to the specified path.
+    /// Serialize settings to TOML and write to the specified path.
     pub fn write(&self, settings_path: &'static str) -> Result<(), Error> {
         fs::write(settings_path, toml::to_string(&self).unwrap())
     }
